d04ea10fda2d2c8d2d0d7428035f1dc961e5e3d8
unknown <thesoupissoup@gmail.com>
2018-06-07 17:50:01 -0700

    eh

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,6 +1,6 @@
 var start = alert("");
 
-var topics = ["lion", "tiger", "bear", "wolf", "snake", "mouse", "gilamonster", "monkey"]
+var topics = ["lion", "tiger", "bear", "wolf", "snake", "mouse", "monkey"]
 //6 topics//
 var one = 0;
 console.log(topics);
@@ -14,12 +14,13 @@ function makebuttons() {
         //grab = button.val($(this));
 
         button.on("click", function () {
-            //$("#huh").text("inch");
-            $("#gifField").html("HEY");
+            $("#huh").text("inch");
+            //retrieve = button.val();
 
+            buttonchoice = $(this).html();
             var APIKey = "MKx7Mjp2X4LMvUfknqdeT7tyc1WtMsO7";
 
-            queryURL = "https://api.giphy.com/v1/gifs/search?q=" + button.val(this) + "&api_key=" + APIKey + "&limit=5";
+            queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonchoice + "&api_key=" + APIKey + "&limit=5";
 
             $.ajax({
                 url: queryURL,
@@ -30,12 +31,16 @@ function makebuttons() {
 
                     console.log(queryURL);
                     console.log(response);
+
                     results = response.data;
+
                     for (var i = 0; i < results.length; i++) {
-                        var animalDiv = $("<div>");
+                        var gifDiv = $("<div>");
+                        gifDiv.addClass("item");
                         var animalImage = $("<img>");
                         animalImage.attr("src", results[i].images.fixed_height.url);
-                        animalDiv.append(animalImage);
+                        gifDiv.append(animalImage);
+                        $("#GifField").prepend(gifDiv);
                     };
                 });
 
@@ -49,103 +54,3 @@ function makebuttons() {
 };
 
 makebuttons();
-
-
-
-
-
-
-
-var gifspot = $("#gifField");
-
-
-
-
-//var  = topics[3];
-
-var APIKey = "MKx7Mjp2X4LMvUfknqdeT7tyc1WtMsO7";
-
-queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "tiger" + "&api_key=" + APIKey + "&limit=5";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-    .then(function (response) {
-
-
-        console.log(queryURL);
-        console.log(response);
-        results = response.data;
-        for (var i = 0; i < results.length; i++) {
-            var animalDiv = $("<div>");
-            var animalImage = $("<img>");
-            animalImage.attr("src", results[i].images.fixed_height.url);
-            animalDiv.append(animalImage);
-        };
-    });
-
-
-
-
-
-
-
-
-// function makebuttons() {
-//     for (i = 0; i < topics.length; i++) {
-//         var button = $("<button>").html(topics[i]);
-//         button.addClass("buttonstyle");
-
-//         button.on("click", function () {
-//             $("#huh").text(button.val(this));
-
-//             //var animal = $(this).attr("data-animal");
-
-
-
-
-
-
-//             $("#buttonfield").append(button);
-//         }
-//     );
-
-//         makebuttons();
-
-//         var APIKey = "MKx7Mjp2X4LMvUfknqdeT7tyc1WtMsO7";
-//         // //"&limit=5"
-//         queryURL = "https://api.giphy.com/v1/gifs/search?q=" + button + "&api_key=" + APIKey;
-
-//         $.ajax({
-//             url: queryURL,
-//             method: "GET"
-//         })
-//             .then(function (response) {
-//                 console.log(queryURL);
-//                 console.log(response);
-                //var results = response.data;
-                // for (var i = 0; i < results.length; i++)
-                //     var animaldiv = $("<div>");
-                // var animalImage = $("<img>");
-                // animalImage.attr("src", results[i].images.fixed_height.url);
-                // animalDiv.append(animalImage);
-                // $("#gifField").prepend(animalDiv);
-            //});
-
-
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // })
-        //     .then(function (response) {
-        //         console.log(queryURL);
-        //         console.log(response);
-        //         var results = response.data;
-        //         for (var i = 0; i < results.length; i++)
-        //             var animaldiv = $("<div>");
-        //         var animalImage = $("<img>");
-        //         animalImage.attr("src", results[i].images.fixed_height.url);
-        //         animalDiv.append(animalImage);
-        //         $("#gifField").prepend(animalDiv);
-        //     });
